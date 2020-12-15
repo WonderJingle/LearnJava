ea3be064a0585d6ac8304cd672f33aec4a39d3af
WonderJingle <74862112+WonderJingle@users.noreply.github.com>
2020-12-14 19:26:28 -1200

    Update Object_Oriented_Programming_Concepts_EX1_EX2.java

--- Object_Oriented_Programming_Concepts_EX1_EX2.java
+++ Object_Oriented_Programming_Concepts_EX1_EX2.java
@@ -1,4 +1,7 @@
 // Reference document https://docs.oracle.com/javase/tutorial/java/concepts/class.html
+// https://docs.oracle.com/javase/tutorial/java/concepts/interface.html
+// https://docs.oracle.com/javase/tutorial/java/concepts/QandE/questions.html
+// Exercises1:Create new classes for each real-world object that you observed at the beginning of this trail. Refer to the Bicycle class if you forget the required syntax.
 class dog {
 
     int happy = 0;
@@ -17,6 +20,7 @@ class dog {
     }
 }
 
+//Exercises2:For each new class that you've created above, create an interface that defines its behavior, then require your class to implement it. Omit one or two methods and try compiling. What does the error look like?
 interface dog {
     
      void timeonplaying(int newValue);
