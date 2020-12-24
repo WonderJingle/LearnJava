6d55b536b1d7e0c4040d328072b4213720028c86
WonderJingle <74862112+WonderJingle@users.noreply.github.com>
2020-12-23 15:18:52 -1200

    Add files via upload

---
+++ weekone.java
@@ -0,0 +1,18 @@
+package nihao;
+
+import java.util.Scanner;
+
+public class weekone {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int f;
+        double c;
+        Scanner in = new Scanner(System.in);
+        f = in.nextInt();
+        c = (f-32)*(5/9.0);
+        System.out.println((int)c);
+        
+	}
+
+}
