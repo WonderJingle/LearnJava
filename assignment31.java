0852ba3ccd61b47c1f2a14f7963219ad53415658
WonderJingle <74862112+WonderJingle@users.noreply.github.com>
2020-12-30 13:45:52 -1200

    Add files via upload

---
+++ assignment31.java
@@ -0,0 +1,57 @@
+package week3loop;
+//think about how you solve this as a human? convert it into sentence, convert it into steps, convert it into algorithm.
+import java.util.Scanner;
+
+public class assignment31 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+// 		initialization
+		Scanner in = new Scanner(System.in);
+		int number = 0;		//think about what variable you will use? and how many?
+		int odd = 0;
+		int even = 0;
+//		use do-while to solve this
+//			do {
+//				number = in.nextInt();
+//				if ( number > 0 & number < 100000)
+//				{
+//					if ( number != 2*(number/2) )
+//					{
+//						odd = odd+1;
+//					}
+//					else
+//					{
+//						even = even+1;
+//					}
+//				}
+//				else if (number != -1)
+//				{
+//					System.out.println(number + " is out of range!");
+//				}
+//			} while (number != -1);
+//		use while to solve this
+		number = in.nextInt();
+		while (number != -1)
+		{
+			if ( number > 0 & number < 100000)
+				{
+					if ( number != 2*(number/2) )
+					{
+						odd = odd+1;
+					}
+					else
+					{
+						even = even+1;
+					}
+				}
+				else if (number != -1)
+				{
+					System.out.println(number + " is out of range!");
+				}
+			number = in.nextInt();
+		}
+		System.out.print(odd + " " +even);
+	}
+
+}
